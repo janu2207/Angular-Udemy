dfc739b780fd0ed3b08ffc83cc40708fdc320411
Jahnavi Bantupalli <jbantupa@asu.edu>
2019-04-10 16:24:28 -0700

    end of course

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,8 +1,13 @@
-import { CanActivate } from '@angular/router';
+import { CanActivate, RouterStateSnapshot } from '@angular/router';
 import { ActivatedRouteSnapshot } from '@angular/router';
-
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+@Injectable()
 export class AuthGuard implements CanActivate{
-    canActivate(route:ActivatedRouteSnapshot){
+    constructor(private authService:AuthService){}
 
+    canActivate(route:ActivatedRouteSnapshot,state:RouterStateSnapshot){
+        return this.authService.isAuthenticated();
     }
+    
 }
